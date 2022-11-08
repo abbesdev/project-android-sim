a93ae9bea0a40a23a6d213c09828f8fdbf65137a
abbesdev <abbesmohamed45@gmail.com>
2022-11-08 19:27:58 +0100

    pushed progress

--- app/src/main/java/com/abbes/schoolspace/SignInScreen.kt
+++ app/src/main/java/com/abbes/schoolspace/SignInScreen.kt
@@ -1,13 +1,21 @@
 package com.abbes.schoolspace
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SignInScreen  : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.signin_screen)
+        var buttonSignup = findViewById<TextView>(R.id.textView2)
 
+        buttonSignup.setOnClickListener({
+            var intent = Intent(applicationContext, SignUpScreen::class.java)
+            startActivity(intent)
+        })
 
     }
 }
