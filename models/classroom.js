99c6cca7050c6f52021664da42c4afe9ac9e70e9
abbesdev <abbesmohamed45@gmail.com>
2022-12-03 03:23:17 +0100

    added classroom controller and routes to server

---
+++ models/classroom.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+var Classroom = mongoose.model(
+  "Classroom",
+  new mongoose.Schema({
+    classroomTitle:String,
+
+    
+    classes: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Classes"
+        }
+      ],
+      students: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User"
+        }
+      ],
+      teacher: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User"
+        }
+      ],
+      subject: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Subject"
+        }
+      ],
+
+    
+    
+   
+  
+   
+  })
+);
+
+module.exports = Classes;
