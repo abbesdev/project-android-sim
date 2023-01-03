26b2f4014eae8173a3456e8cbe223a1694124500
abbesdev <abbesmohamed45@gmail.com>
2023-01-03 02:41:28 +0100

    fixed post attributes for classroom

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -68,7 +68,7 @@ exports.signup = (req, res) => {
               },
             );
 /**************** */
-            res.send({ message: "User was registered successfully!" });
+            res.send({ user : user.id });
           });
         }
       );
@@ -86,7 +86,7 @@ exports.signup = (req, res) => {
             return;
           }
 
-          res.send({ message: "User was registered successfully!" });
+          res.send({ user : user.id });
         });
       });
     }
